78ac1e4766a3a57427ec23a697808e79207f1c33
Minyu Li <minyuli1997@gmail.com>
2018-07-16 15:26:22 +0200

    wow

---
+++ GifsOnly/routes/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const passport = require('passport');
+const authRoutes = express.Router();
+const User = require("../models/User");
+const multer = require('multer');
+const upload = multer({ dest: './public/uploads/' });
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+authRoutes.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", upload.single("photo"),(req, res, next) => {
+  //console.log(req);
+  const { username, password, email } = req.body;
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashPass,
+      profileGif: req.file ? req.file.filename : ""
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = authRoutes;
